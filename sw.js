ce8bfb8a842b25e76612ffef6108cf51fe1d1d93
Tim Volodine <timvolodine@google.com>
2015-09-04 19:40:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,12 @@
 
 addEventListener('notificationclick', function(event) {
   //var url = event.notification.data.url;
-  var data = event.notification.title;
-  console.log('HA -- '+data);
+  var title = event.notification.title;
+  console.log('HA -- ' + title);
   //vibrate(300);
   //alert('click:'+data);
   //event.notification.close();
-  registration.showNotification("REPLY bla");
+  registration.showNotification("REPLY " + title);
   //event.waitUntil(clients.openWindow('https://timvolodine.github.io/notifications/'));
 });
 
