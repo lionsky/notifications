6513eb19f5f7373a4c051a48155179d3517b5286
Tim Volodine <timvolodine@google.com>
2015-09-04 14:04:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ addEventListener('notificationclick', function(event) {
   //var url = event.notification.data.url;
   var data = event.notification.title;
   console.log('HA -- '+data);
-  //alert('click:'+url);
+  alert('click:'+data);
   event.notification.close();
   //event.waitUntil(clients.openWindow(url));
 });
