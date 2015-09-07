663c242eafd7a11fc010ed7f91a9cda024c7fb7a
Tim Volodine <timvolodine@google.com>
2015-09-07 13:53:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 addEventListener('notificationclick', function(event) {
   //var url = event.notification.data.url;
   var title = event.notification.title;
-  console.log('HA -- ' + title);
+  console.log('notificationclick -- ' + title);
   //vibrate(300);
   //alert('click:'+data);
   //event.notification.close();
